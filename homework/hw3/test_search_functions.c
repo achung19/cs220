be3f6bda73a9fd93711cb659b6d7af26d5f18516
Alisen Chung <achung19@jhu.edu>
2019-09-25 21:33:43 -0400

    added tests for all helper functions

--- homework/hw3/test_search_functions.c
+++ homework/hw3/test_search_functions.c
@@ -20,7 +20,7 @@ void test_file_eq();      // This one is already fully defined below.
 void test_convert_lowercase();
 void test_populate_grid();
 void test_find_at_horizontal();
-void test_find_at_vertical()
+void test_find_at_vertical();
 void test_find_right();
 void test_find_left();
 void test_find_down();
@@ -87,19 +87,19 @@ void test_file_eq() {
 
 void test_convert_lowercase(){
   
-  char test1 = "TESTING";
-  char test2 = "Testing";
-  char test3 = "testing";
-  char err1 = "!@#$%";
-  char err2 = "test err";
-  char err3 = "Abc!@#";
-  char err4 = "123";
+  char test1[] = "TESTING";
+  char test2[] = "Testing";
+  char test3[] = "testing";
+  char err1[] = "!@#$%";
+  char err2[] = "test err";
+  char err3[] = "Abc!@#";
+  char err4[] = "123";
   
   int test1_val = convert_lowercase(test1);
   int test2_val = convert_lowercase(test2);
   int test3_val = convert_lowercase(test3);
   int err1_val = convert_lowercase(err1);
-  int err2_val = onvert_lowercase(err2);
+  int err2_val = convert_lowercase(err2);
   int err3_val = convert_lowercase(err3);
   int err4_val = convert_lowercase(err4);
   
@@ -109,60 +109,111 @@ void test_convert_lowercase(){
   assert(test1_val == 0);
   assert(test2_val == 0);
   assert(test3_val == 0);
-  assert(err1 == -1);
-  assert(err2 == -1);
-  assert(err3 == -1);
-  assert(err4 == -1);
+  assert(err1_val == -1);
+  assert(err2_val == -1);
+  assert(err3_val == -1);
+  assert(err4_val == -1);
 }
 
 void test_populate_grid(){
+  FILE* file = fopen("testgrid.txt", "w");
+  fprintf(file, "abc\ndef\nghi\n\n");
+  fclose(file);
 
-  assert(1);  //replace this stub!
+  char test_grid[MAX_SIZE][MAX_SIZE];
+  int n = populate_grid(test_grid, "testgrid.txt");
 
+  assert(n == 3);
+  assert(test_grid[0][0] == 'a');
+  assert(test_grid[0][1] == 'b');
+  assert(test_grid[0][2] == 'c');
+  assert(test_grid[1][0] == 'd');
+  assert(test_grid[1][1] == 'e');
+  assert(test_grid[1][2] == 'f');
+  assert(test_grid[2][0] == 'g');
+  assert(test_grid[2][1] == 'h');
+  assert(test_grid[2][2] == 'i');  
 }
 
 void test_find_at_horizontal(){
+  char test_grid[MAX_SIZE][MAX_SIZE] = {{'a', 'b', 'c'}};
+  int temp[] = {0,0};
+  int bad_temp[] = {1, 0};
 
-  assert(1);
-
+  assert(find_at_horizontal(test_grid, 3, "abc", temp) == 1);
+  assert(find_at_horizontal(test_grid, 3, "aaa", temp) == 0);
+  assert(find_at_horizontal(test_grid, 3, "aaaaaa", temp) == 0);
+  assert(find_at_horizontal(test_grid, 3, "abc", bad_temp) == 0);
 }
 
 void test_find_at_vertical(){
+  char test_grid[MAX_SIZE][MAX_SIZE] = {{'a'}, {'b'}, {'c'}};
+  int temp[] = {0,0};
+  int bad_temp[] = {0, 1};
 
-  assert(1);
-
+  assert(find_at_vertical(test_grid, 3, "abc", temp) == 1);
+  assert(find_at_vertical(test_grid, 3, "aaa", temp) == 0);
+  assert(find_at_vertical(test_grid, 3, "aaaaaa", temp) == 0);
+  assert(find_at_vertical(test_grid, 3, "abc", bad_temp) == 0);
 }
 
 void test_find_right(){
-
-  assert(1);  //replace this stub!
-
+  FILE* output_file = fopen("output_test.txt", "w");
+  char test_grid[MAX_SIZE][MAX_SIZE] = {{'a', 'b', 'c'}, {'d', 'e', 'f'}, {'a', 'b', 'c'}};
+  
+  assert(find_right(test_grid, 3, "abc", output_file) == 2);
+  assert(find_right(test_grid, 3, "def", output_file) == 1);
+  assert(find_right(test_grid, 3, "ghi", output_file) == 0);
+  assert(find_right(test_grid, 3, "zz", output_file) == 0);
 }
-
 
 void test_find_left(){
-
-  assert(1);  //replace this stub!
+  FILE* output_file = fopen("output_test.txt", "w");
+  char test_grid[MAX_SIZE][MAX_SIZE] = {{'a', 'b', 'c'}, {'d', 'e', 'f'}, {'a', 'b', 'c'}};
+  
+  assert(find_left(test_grid, 3, "cba", output_file) == 2);
+  assert(find_left(test_grid, 3, "fed", output_file) == 1);
+  assert(find_left(test_grid, 3, "ihg", output_file) == 0);
+  assert(find_left(test_grid, 3, "zz", output_file) == 0);
 }
-
 
 void test_find_down(){
-
-  assert(1);  //replace this stub!
-
+  FILE* output_file = fopen("output_test.txt", "w");
+  char test_grid[MAX_SIZE][MAX_SIZE] = {{'a', 'd', 'a'}, {'b', 'e', 'b'}, {'c', 'f', 'c'}};
+  
+  assert(find_down(test_grid, 3, "abc", output_file) == 2);
+  assert(find_down(test_grid, 3, "def", output_file) == 1);
+  assert(find_down(test_grid, 3, "ghi", output_file) == 0);
+  assert(find_down(test_grid, 3, "zz", output_file) == 0);
 }
 
-
 void test_find_up(){
-
-  assert(1);  //replace this stub!
-
+  FILE* output_file = fopen("output_test.txt", "w");
+  char test_grid[MAX_SIZE][MAX_SIZE] = {{'a', 'd', 'a'}, {'b', 'e', 'b'}, {'c', 'f', 'c'}};
+  
+  assert(find_up(test_grid, 3, "cba", output_file) == 2);
+  assert(find_up(test_grid, 3, "fed", output_file) == 1);
+  assert(find_up(test_grid, 3, "ihg", output_file) == 0);
+  assert(find_up(test_grid, 3, "zz", output_file) == 0);
 }
 
 
 void test_find_all(){
-
-  assert(1);  //replace this stub!
-
+  FILE* output_file = fopen("output_test.txt", "w");
+  char test_grid[MAX_SIZE][MAX_SIZE] = {{'a', 'b', 'c'}, {'d', 'e', 'f'}, {'g', 'h', 'i'}};
+  
+  assert(find_all(test_grid, 3, "abc", output_file) == 1);
+  assert(find_all(test_grid, 3, "fed", output_file) == 1);
+  assert(find_all(test_grid, 3, "adg", output_file) == 1);
+  assert(find_all(test_grid, 3, "heb", output_file) == 1);
+  assert(find_all(test_grid, 3, "def", output_file) == 1);
+  assert(find_all(test_grid, 3, "ifc", output_file) == 1);
+  assert(find_all(test_grid, 3, "cfi", output_file) == 1);
+  assert(find_all(test_grid, 3, "cdg", output_file) == 0);
+  assert(find_all(test_grid, 3, "aeh", output_file) == 0);
+  assert(find_all(test_grid, 3, "cfg", output_file) == 0);
+  assert(find_all(test_grid, 3, "fea", output_file) == 0);
+  assert(find_all(test_grid, 3, "abg", output_file) == 0);
+  assert(find_all(test_grid, 3, "zz", output_file) == 0);
 }
 
